14d96b8bc38bbf1afd5add99aa871ad4bf1a77a4
ymaa0063@student.monash.edu <64070414+1029YanMa@users.noreply.github.com>
2020-08-25 20:31:02 +1000

    update analysis part Q1-Q4

---
+++ token.R
@@ -0,0 +1,16 @@
+id
+ff863bd8858d482482525979e9fd968a
+secret
+8218b9458766435481d8d61b53b670c7
+
+redirect
+https://github.com/1029YanMa
+url encode
+http%3A%2F%2Flocalhost%3A8888%2Fcallback
+
+id&secret encode
+ff863bd8858d482482525979e9fd968a:8218b9458766435481d8d61b53b670c7
+
+ZmY4NjNiZDg4NThkNDgyNDgyNTI1OTc5ZTlmZDk2OGE6ODIxOGI5NDU4NzY2NDM1NDgxZDhkNjFiNTNiNjcwYzc=
+
+https://accounts.spotify.com/authorize?client_id=ff863bd8858d482482525979e9fd968a&response_type=code&redirect_uri=get-my-songs-login%3A%2F%2Fcallback&scope=playlist-read-private
